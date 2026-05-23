2b4d1404398b0a6ada8dfadf31c271cb16e1c88f
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2026-05-23 16:05:54 +0200

    style: Refactor ViT module for improved readability and style consistency

--- src/model/vit.py
+++ src/model/vit.py
@@ -2,15 +2,13 @@ import torch
 from torch import nn
 from torch.nn import Module, ModuleList
 
-# helpers
 
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-# classes
 
 class FeedForward(Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
+    def __init__(self, dim, hidden_dim, dropout=0.0):
         super().__init__()
         self.net = nn.Sequential(
             nn.LayerNorm(dim),
@@ -18,38 +16,42 @@ class FeedForward(Module):
             nn.GELU(),
             nn.Dropout(dropout),
             nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
         return self.net(x)
 
+
 class Attention(Module):
-    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.0):
         super().__init__()
-        inner_dim = dim_head *  heads
+        inner_dim = dim_head * heads
         project_out = not (heads == 1 and dim_head == dim)
 
         self.heads = heads
-        self.scale = dim_head ** -0.5
+        self.scale = dim_head**-0.5
 
         self.norm = nn.LayerNorm(dim)
 
-        self.attend = nn.Softmax(dim = -1)
+        self.attend = nn.Softmax(dim=-1)
         self.dropout = nn.Dropout(dropout)
 
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
 
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
-            nn.Dropout(dropout)
-        ) if project_out else nn.Identity()
+        self.to_out = (
+            nn.Sequential(nn.Linear(inner_dim, dim), nn.Dropout(dropout))
+            if project_out
+            else nn.Identity()
+        )
 
     def forward(self, x):
         x = self.norm(x)
 
-        qkv = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: t.unflatten(-1, (self.heads, -1)).transpose(1, 2), qkv)
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(
+            lambda t: t.unflatten(-1, (self.heads, -1)).transpose(1, 2), qkv
+        )
 
         dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
 
@@ -60,17 +62,24 @@ class Attention(Module):
         out = out.transpose(1, 2).flatten(2)
         return self.to_out(out)
 
+
 class Transformer(Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.0):
         super().__init__()
         self.norm = nn.LayerNorm(dim)
         self.layers = ModuleList([])
 
         for _ in range(depth):
-            self.layers.append(ModuleList([
-                Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout),
-                FeedForward(dim, mlp_dim, dropout = dropout)
-            ]))
+            self.layers.append(
+                ModuleList(
+                    [
+                        Attention(
+                            dim, heads=heads, dim_head=dim_head, dropout=dropout
+                        ),
+                        FeedForward(dim, mlp_dim, dropout=dropout),
+                    ]
+                )
+            )
 
     def forward(self, x):
         for attn, ff in self.layers:
@@ -78,6 +87,7 @@ class Transformer(Module):
             x = ff(x) + x
 
         return self.norm(x)
+
 
 class PatchRearrange(Module):
     def __init__(self, patch_height, patch_width):
@@ -87,23 +97,47 @@ class PatchRearrange(Module):
 
     def forward(self, x):
         b, c, h, w = x.shape
-        x = x.unflatten(2, (h // self.ph, self.ph)).unflatten(4, (w // self.pw, self.pw))
+        x = x.unflatten(2, (h // self.ph, self.ph)).unflatten(
+            4, (w // self.pw, self.pw)
+        )
         x = x.permute(0, 2, 4, 3, 5, 1)
         return x.reshape(b, -1, self.ph * self.pw * c)
 
+
 class ViT(Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+    def __init__(
+        self,
+        *,
+        image_size,
+        patch_size,
+        num_classes,
+        dim,
+        depth,
+        heads,
+        mlp_dim,
+        pool="cls",
+        channels=3,
+        dim_head=64,
+        dropout=0.0,
+        emb_dropout=0.0,
+    ):
         super().__init__()
         image_height, image_width = pair(image_size)
         self.patch_size = patch_height, patch_width = pair(patch_size)
 
-        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        assert (
+            image_height % patch_height == 0 and image_width % patch_width == 0
+        ), "Image dimensions must be divisible by the patch size."
 
-        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        num_patches = (image_height // patch_height) * (
+            image_width // patch_width
+        )
         patch_dim = channels * patch_height * patch_width
 
-        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
-        num_cls_tokens = 1 if pool == 'cls' else 0
+        assert pool in {"cls", "mean"}, (
+            "pool type must be either cls (cls token) or mean (mean pooling)"
+        )
+        num_cls_tokens = 1 if pool == "cls" else 0
 
         self.to_patch_embedding = nn.Sequential(
             PatchRearrange(patch_height, patch_width),
@@ -113,11 +147,15 @@ class ViT(Module):
         )
 
         self.cls_token = nn.Parameter(torch.randn(num_cls_tokens, dim))
-        self.pos_embedding = nn.Parameter(torch.randn(num_patches + num_cls_tokens, dim))
+        self.pos_embedding = nn.Parameter(
+            torch.randn(num_patches + num_cls_tokens, dim)
+        )
 
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.transformer = Transformer(
+            dim, depth, heads, dim_head, mlp_dim, dropout
+        )
 
         self.pool = pool
         self.to_latent = nn.Identity()
@@ -129,7 +167,7 @@ class ViT(Module):
         x = self.to_patch_embedding(img)
 
         cls_tokens = self.cls_token.expand(batch, -1, -1)
-        x = torch.cat((cls_tokens, x), dim = 1)
+        x = torch.cat((cls_tokens, x), dim=1)
 
         seq = x.shape[1]
 
@@ -141,7 +179,7 @@ class ViT(Module):
         if self.mlp_head is None:
             return x
 
-        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+        x = x.mean(dim=1) if self.pool == "mean" else x[:, 0]
 
         x = self.to_latent(x)
         return self.mlp_head(x)
